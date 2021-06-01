62fd24be1f22308500dc82458027f11d16846926
sigintlpinvest <85190474+sigintlpinvest@users.noreply.github.com>
2021-06-02 02:15:33 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -45,3 +45,14 @@ echo $result . "\r\n";
 }
 }
 echo "OK";
+<?php
+require "vendor/autoload.php";
+$access_token = 'q/+IeuRV/AripP3j5yNeboiQQyQMgBq+1ZHSf6CCdnrvcmv37R8QNxJ2WxRstlbapfw0Vv+G4J308G0FvZJlAdfCyMNIZ9c/KEvOusLS1LYiU7TGnQT2dYgTZQNkeDt2oRm3Y9CtxVXvpbS1Pa+YKwdB04t89/1O/w1cDnyilFU=';
+$channelSecret = '20664390659bac6e90d77bd270cdc83a';
+$idPush = '1656053933'
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
+$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello world');
+$response = $bot->pushMessage($idPush, $textMessageBuilder);
+
+echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
